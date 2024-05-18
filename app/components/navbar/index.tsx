759aec61cf6414e390d20c9a2863e61a7dc17e7f
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-05-18 12:48:31 -0400

    Massive upgrade to the look of the site

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { Icon } from "./indexIcon";
+import Typewriter from "react-ts-typewriter";
+{
+  /* 
+     This allows us to create the navbar itself
+     We import our Icon from indexIcon so we can use boxIcons without any issue
+     What we wanted was to create a traditonal navigation bart but what i decided to do was just impement a personal touch to the webpage
+  */
+}
+const Navbar = () => {
+  return (
+    <>
+      {/* 
+            This creatse the divider where we have the darkness gradient color
+        */}
+        
+      <div className="w-full h-20 bg-gradient-to-r from-zinc-900 to-slate-700 sticky top-0">
+        {/* 
+            This creates another divider which is meant as a container
+        */}
+
+        <div className="container mx-auto flex justify-between items-center">
+          {/* 
+            This creates our name on the left of the screen
+        */}
+          <a
+            href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.youtube.com/watch%3Fv%3D_NfcYjPq-RE&ved=2ahUKEwjY_7KUrJeGAxUHD1kFHQPkDSQQtwJ6BAhBEAI&usg=AOvVaw17MX1V-12iSIjYTJTQQUaU"
+            className="text-2xl font-bold hover:text-gray-300"
+          >
+            Nelson Alvarez
+          </a>
+           {/* 
+            Within here is where we create the interesting navbar where it shows a unique grep command for my GitHub username and then has the three folders they can go to which are direct links to my social medias and email
+        */}
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <Typewriter
+              text='Grep -R "DebuggingaHopper"'
+              delay={5500}
+              loop={true}
+            />
+            <a className="hover:text-gray-300" href="https://github.com/DebuggingaHopper">
+            <code className="font-mono font-bold">  /<Icon name="bx bxl-github" size="16px" />Github </code>
+            </a>
+
+            <a className="hover:text-gray-300" href="https://www.linkedin.com/in/nelson-alvarez-62027b189">
+            <code className="font-mono font-bold">  /<Icon name="bx bxl-linkedin-square" size="16px" />WorkForce </code>
+            </a>
+
+            <a className="hover:text-gray-300" href="mailto:“nelson.j.alvarez01@gmail.com">
+            <code className="font-mono font-bold">  /<Icon name="bx bxs-envelope" size="16px" />Contact/ </code>
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24  bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Grep -R "DebuggingaHopper" 
-          <code className="font-mono font-bold"> /WorkForce/</code>
-        </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -15,7 +11,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            {/* 
+           So what the following did was created By and displayed the vercel logo
+
+           By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -24,10 +23,205 @@ export default function Home() {
               height={24}
               priority
             />
+          */}
           </a>
         </div>
       </div>
 
+      <div className="relative z-[-1] border-solid border-2 flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]  ">
+        <div className="m-auto flex max-w-screen-xl flex-col items-center gap-6 p-6 lg:flex-row lg:gap-0 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <div className="relative flex w-full flex-1 items-center lg:basis-[500px] lg:text-right ">
+            <div className="flex-1 lg:pr-6">
+              <h2 className="text-4xl font-bold">Refinement Every Day</h2>
+              <h3 className="text-1xl text-center">
+                <br></br>
+                <br></br>Employers seeking a versatile expert in hospital
+                systems, Oracle Databases, mobile app development, and server
+                maintenance, look no further.<br></br>
+                <br></br>I'm Nelson Josue Alvarez, at 22, I offer a potent
+                combination of youthful vigor and seasoned proficiency to
+                enhance your team's capabilities.
+              </h3>
+            </div>
+            <div className="top-1/2 mt-px hidden h-px w-16 bg-green-500 lg:block"></div>
+          </div>
+
+          <ul className="lg:basis-[750px]">
+            <li className="group relative py-6">
+              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
+              <div className="items-center gap-6 lg:flex">
+                <div className="relative mb-4 lg:mb-0 lg:pl-16">
+                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
+                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
+                    <svg
+                      className="m-auto block h-full max-h-12 w-full"
+                      fill="none"
+                      height="15"
+                      width="15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
+                        fill="white"
+                        fill-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="mb-2 text-xl font-bold">ChargersLink</h3>
+                  <p className="opacity-75">
+                    A Cross-Platform Mobile Application for Capitol
+                    Technology University aimed at improving
+                    student life on and off campus. The primary goal was for this application to have the student's needs met which involved alot of user input in implementing shcedules, resources, and news that the student community desired.
+                    </p>
+                </div>
+              </div>
+            </li>
+            <li className="group relative py-6">
+              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
+              <div className="items-center gap-6 lg:flex">
+                <div className="relative mb-4 lg:mb-0 lg:pl-16">
+                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
+                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
+                    <svg
+                      className="m-auto block h-full max-h-12 w-full"
+                      fill="none"
+                      height="15"
+                      width="15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
+                        fill="white"
+                        fill-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="mb-2 text-xl font-bold"> CapTech Fitness Map</h3>
+                  <p className="opacity-75">
+                    This was a project meant as a predecessor prior to Chargers Link which displayed the activites that can be conducted on campus alongside pormoting physcial exercise. This is an ongoing project to improve even further after Chargers Link Development from last year
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="group relative py-6">
+              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
+              <div className="items-center gap-6 lg:flex">
+                <div className="relative mb-4 lg:mb-0 lg:pl-16">
+                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
+                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
+                    <svg
+                      className="m-auto block h-full max-h-12 w-full"
+                      fill="none"
+                      height="15"
+                      width="15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
+                        fill="white"
+                        fill-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="mb-2 text-xl font-bold">
+                    Inbound & outbound Interface design for Hospital Information Software
+                  </h3>
+                  <p className="opacity-75">
+                    Have constructed and mantained Interfaces that integrate with Hopsital information Systems such as EPIC, this is part of my regular duties at my current position where i conduct varying tasks to ensure the satisfaction of various Hopsitals.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="group relative py-6">
+              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
+              <div className="items-center gap-6 lg:flex">
+                <div className="relative mb-4 lg:mb-0 lg:pl-16">
+                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
+                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
+                    <svg
+                      className="m-auto block h-full max-h-12 w-full"
+                      fill="none"
+                      height="15"
+                      width="15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
+                        fill="white"
+                        fill-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="mb-2 text-xl font-bold">
+                    Portfolio
+                  </h3>
+                  <p className="opacity-75">
+                    This portfolio page was constructed as a personal project to refine my skills and test the waters in the development of my first website.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li className="group relative py-6">
+              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
+              <div className="items-center gap-6 lg:flex">
+                <div className="relative mb-4 lg:mb-0 lg:pl-16">
+                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
+                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
+                    <svg
+                      className="m-auto block h-full max-h-12 w-full"
+                      fill="none"
+                      height="15"
+                      width="15"
+                      viewBox="0 0 15 15"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clip-rule="evenodd"
+                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
+                        fill="white"
+                        fill-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                <div>
+                  <h3 className="mb-2 text-xl font-bold">
+                    Elevar
+                  </h3>
+                  <p className="opacity-75">
+                    Among the Hispanic American community many of the members within our community have relatives within Small business or Roofing, so this is an application made to serve their needs in project management, recording and communication]
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* 
+           So what the following did was place the image in the center what we want is to create a center card with our information
+          
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -38,8 +232,10 @@ export default function Home() {
           priority
         />
       </div>
+            />
+          */}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -47,13 +243,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            Resume{" "}
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            Find my resume within here and feel free to reach out!
           </p>
         </a>
 
@@ -64,13 +257,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            Published Paper{" "}
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            At the age of 22 I was able to work with a team to have <i>Crowdfunding Fraud Prevention using Smart Contracts</i> published in the 2023 IITCEE
           </p>
         </a>
 
@@ -81,30 +271,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Hospital Experience{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Have had the pelasure to work with hopsitals in cosntructing solutions to improve their workflow, alongside ensuring pateint information is secured.
           </p>
         </a>
       </div>
