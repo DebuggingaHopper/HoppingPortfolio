de8e90947f89c4aed400b143eff92f78fb07fd83
debuggingahopper <nelson.j.alvarez01@gmail.com>
2024-05-18 18:21:43 -0400

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
-
-
+import FitnessIcon from "/public/Fitness.svg";
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24  bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {/* 
-           So what the following did was created By and displayed the vercel logo
-
-           By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          */}
-          </a>
-        </div>
-      </div>
-
       <div className="relative z-[-1] border-solid border-2 flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]  ">
         <div className="m-auto flex max-w-screen-xl flex-col items-center gap-6 p-6 lg:flex-row lg:gap-0 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <div className="relative flex w-full flex-1 items-center lg:basis-[500px] lg:text-right ">
@@ -53,31 +28,28 @@ export default function Home() {
                 <div className="relative mb-4 lg:mb-0 lg:pl-16">
                   <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
                   <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
-                    <svg
-                      className="m-auto block h-full max-h-12 w-full"
-                      fill="none"
-                      height="15"
-                      width="15"
-                      viewBox="0 0 15 15"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        clip-rule="evenodd"
-                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
-                        fill="white"
-                        fill-rule="evenodd"
-                      />
-                    </svg>
+                    <Image
+                      className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert m-auto block h-full max-h-full w-10"
+                      src="/Chargers.svg"
+                      alt="Chargers Link"
+                      width={180}
+                      height={60}
+                      priority
+                    />
                   </div>
                 </div>
 
                 <div>
                   <h3 className="mb-2 text-xl font-bold">ChargersLink</h3>
-                  <p className="opacity-75">
-                    A Cross-Platform Mobile Application for Capitol
-                    Technology University aimed at improving
-                    student life on and off campus. The primary goal was for this application to have the student's needs met which involved alot of user input in implementing shcedules, resources, and news that the student community desired.
-                    </p>
+                  <p className="opacity-75 text-white">
+                    A Cross-Platform Mobile Application for Capitol Technology
+                    University built on Flutter. this application aimed at
+                    improving student life and access to academic resources. The
+                    primary goal was for this application to have the student's
+                    needs met which involved alot of user input in implementing
+                    shcedules, resources, and news that the student community
+                    desired.
+                  </p>
                 </div>
               </div>
             </li>
@@ -87,28 +59,23 @@ export default function Home() {
                 <div className="relative mb-4 lg:mb-0 lg:pl-16">
                   <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
                   <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
-                    <svg
-                      className="m-auto block h-full max-h-12 w-full"
-                      fill="none"
-                      height="15"
-                      width="15"
-                      viewBox="0 0 15 15"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        clip-rule="evenodd"
-                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
-                        fill="white"
-                        fill-rule="evenodd"
-                      />
-                    </svg>
+                    <Image
+                      className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert m-auto block h-full max-h-10 w-full"
+                      src="/portfolio.svg"
+                      alt="Portfolio"
+                      width={180}
+                      height={60}
+                      priority
+                    />
                   </div>
                 </div>
 
                 <div>
-                  <h3 className="mb-2 text-xl font-bold"> CapTech Fitness Map</h3>
-                  <p className="opacity-75">
-                    This was a project meant as a predecessor prior to Chargers Link which displayed the activites that can be conducted on campus alongside pormoting physcial exercise. This is an ongoing project to improve even further after Chargers Link Development from last year
+                  <h3 className="mb-2 text-xl font-bold">Portfolio</h3>
+                  <p className="opacity-75 text-white">
+                    This portfolio page was constructed as a personal project to
+                    refine my skills and test tailwind CSS, Nextjs and
+                    typescript.
                   </p>
                 </div>
               </div>
@@ -138,79 +105,12 @@ export default function Home() {
                 </div>
 
                 <div>
-                  <h3 className="mb-2 text-xl font-bold">
-                    Inbound & outbound Interface design for Hospital Information Software
-                  </h3>
-                  <p className="opacity-75">
-                    Have constructed and mantained Interfaces that integrate with Hopsital information Systems such as EPIC, this is part of my regular duties at my current position where i conduct varying tasks to ensure the satisfaction of various Hopsitals.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className="group relative py-6">
-              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
-              <div className="items-center gap-6 lg:flex">
-                <div className="relative mb-4 lg:mb-0 lg:pl-16">
-                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
-                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
-                    <svg
-                      className="m-auto block h-full max-h-12 w-full"
-                      fill="none"
-                      height="15"
-                      width="15"
-                      viewBox="0 0 15 15"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        clip-rule="evenodd"
-                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
-                        fill="white"
-                        fill-rule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                </div>
-
-                <div>
-                  <h3 className="mb-2 text-xl font-bold">
-                    Portfolio
-                  </h3>
-                  <p className="opacity-75">
-                    This portfolio page was constructed as a personal project to refine my skills and test the waters in the development of my first website.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li className="group relative py-6">
-              <div className="absolute inset-0 hidden w-px bg-green-500 group-first:top-1/2 group-last:bottom-1/2 lg:block"></div>
-              <div className="items-center gap-6 lg:flex">
-                <div className="relative mb-4 lg:mb-0 lg:pl-16">
-                  <div className="absolute left-0 top-1/2 hidden h-px w-full bg-green-500 lg:block"></div>
-                  <div className="relative grid h-24 w-24 rounded-full border border-solid border-green-500 bg-neutral-900">
-                    <svg
-                      className="m-auto block h-full max-h-12 w-full"
-                      fill="none"
-                      height="15"
-                      width="15"
-                      viewBox="0 0 15 15"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        clip-rule="evenodd"
-                        d="M6.0068 13.4178L8.0068 1.4178L8.9932 1.5822L6.9932 13.5822L6.0068 13.4178ZM5.20711 4.5L2.20711 7.5L5.20711 10.5L4.5 11.2071L0.792892 7.5L4.5 3.79289L5.20711 4.5ZM10.5 3.79289L14.2071 7.5L10.5 11.2071L9.79289 10.5L12.7929 7.5L9.79289 4.5L10.5 3.79289Z"
-                        fill="white"
-                        fill-rule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                </div>
-
-                <div>
-                  <h3 className="mb-2 text-xl font-bold">
-                    Elevar
-                  </h3>
-                  <p className="opacity-75">
-                    Among the Hispanic American community many of the members within our community have relatives within Small business or Roofing, so this is an application made to serve their needs in project management, recording and communication]
+                  <h3 className="mb-2 text-xl font-bold">Elevar</h3>
+                  <p className="opacity-75 text-white">
+                    Among the Hispanic American community many of the members
+                    within our community have relatives within Small business or
+                    Roofing, so this is an application made to serve their needs
+                    in project management, recording and communication
                   </p>
                 </div>
               </div>
@@ -219,65 +119,44 @@ export default function Home() {
         </div>
       </div>
 
-      {/* 
-           So what the following did was place the image in the center what we want is to create a center card with our information
-          
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-            />
-          */}
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Resume{" "}
-          </h2>
+          <h2 className="mb-3 text-2xl font-semibold">Published Paper </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            At the age of 22 I was able to work with a team to have{" "}
+            <i>Crowdfunding Fraud Prevention using Smart Contracts</i> published
+            in the 2023 IITCEE
+          </p>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">Resume </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Find my resume within here and feel free to reach out!
           </p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Published Paper{" "}
+            Exposure to the Hospital Sector
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            At the age of 22 I was able to work with a team to have <i>Crowdfunding Fraud Prevention using Smart Contracts</i> published in the 2023 IITCEE
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Hospital Experience{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Have had the pelasure to work with hopsitals in cosntructing solutions to improve their workflow, alongside ensuring pateint information is secured.
+          <p className=" m-0 max-w-[30ch] text-sm opacity-50">
+            Have had the pelasure to work with hopsitals in cosntructing
+            solutions to improve their workflow, suchg as inbound & outbound
+            interfaces that integrate with Hospital Information Systems such as
+            EPIC.
           </p>
         </a>
       </div>
